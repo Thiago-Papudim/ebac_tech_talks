b1eb1b8677b187a9d73bb7fa1ae9e9935956438e
Thiago Soares <thiago_soares_mago@hotmail.com>
2023-10-22 18:18:39 -0300

    criando a contagem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log("Olá mundo")
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaDoEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaDoEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaDoEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaDoEvento % horaEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaDoEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaDoEvento <0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Já aconteceu';
+    }
+}, 1000);
